b95a31d78ab42d9ad3fdea1460849802c84d42d7
Ulisses <ulisses1991@gmail.com>
2024-10-10 17:47:25 -0300

    paineis

---
+++ cypress/e2e/Login/paineis.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+import Login from '../../support/page/login'
+import Home from '../../support/page/home'
+import Helper from '../../support/helpers'
+
+describe('', () => {
+    
+})
--- cypress/support/page/paineis/elements.js
+++ cypress/support/page/paineis/elements.js
@@ -1,3 +1,49 @@
 export const ELEMENTS = {
 
+    PAINEIS_SIDE_MENU_BUTTON: '["data-testid="paineisButton"]',
+    PAINEL_TESTE_HPV_SIDE_MENU: '[data-testid="painelTesteButton"]',
+    VOLTAR_HOME_BUTTON: '[data-testid="returnButton/home"]',
+    TITLE_PAGE_PAINEL_TESTE_HPV: '[data-testid="panel"]',
+    ESTADO_FIELD_INPUT_FILTER: '[data-testid="modalEstadoInput"]',
+    ESTADO_DROPDOWN_BUTTON: '[data-testid="modalEstadoInput"] > .MuiOutlinedInput-root > [data-testid="modalEstadoDropdownButton"] > [data-testid="ArrowDropDownIcon"]',
+    MUNICIPIO_FIELD_INPUT_FILTER: '[data-testid="modalMunicipioInput"]',
+    MUNICIPIO_DROPDOWN_BUTTON: '[data-testid="modalMunicipioDropdownButton"]',
+    ESTABELECIMENTO_FIELD_INPUT_FILTER: '[data-testid="modalEstabelecimentoInput"]',
+    ESTABELECIMENTO_DROPDOWN_BUTTON: '[data-testid="modalEstabelecimentoDropdownButton"]',
+    PERIODO_FIELD_INPUT_FILTER: '[data-testid="periodoInput"]',
+    PERIODO_DROPDOWN_BUTTON: '[data-testid="ArrowDropDownIcon"]',
+    FILTRAR_PAINEL_TESTE_HPV_BUTTON: '[data-testid="submitFilterPanelHpv"]',
+    LIMPAR_FILTROS_BUTTON: '[data-testid="clearFilterPanelHpv"]',
+    LABEL_TESTES_TOTAIS: '[data-testid="totalTestsRealized"]',
+    RESULTADOS_TESTES_NAO_DETECTAVEL_VALUE: '[data-testid="labelAndValueNotDetectabel"]',
+    RESULTADOS_TESTES_NAO_DETECTAVEL_PERCENT: '[data-testid="percentNotDetectable"]',
+    RESULTADOS_TESTES_DETECTAVEL_VALUE: '[data-testid="labelAndValueDetectable"]',
+    RESULTADOS_TESTES_DETECTAVEL_PERCENT: '[data-testid="percentDetectabel"]',
+    RESULTADOS_TESTES_INCONCLUSIVOS_VALUR: '[data-testid="labelAndValueNotConcluded"]',
+    TIPOS_VIRUS_DETECTAVEL_HPV16_VALUE: '[data-testid="labelAndValueHpv16"]',
+    TIPOS_VIRUS_DETECTAVEL_HPV16_PERCENT: '[data-testid="percentHpv16"]',
+    TIPOS_VIRUS_DETECTAVEL_HPV18_VALUE: '[data-testid="labelAndValueHpv18"]',
+    TIPOS_VIRUS_DETECTAVEL_HPV18_PERCENT: '[data-testid="percentHpv18"]',
+    TIPOS_VIRUS_DETECTAVEL_OUTROS_VALUE: '[data-testid="labelAndValueOthers"]',
+    TIPOS_VIRUS_DETECTAVEL_OUTROS_PERCENT: '[data-testid="percentOthers"]',
+    TESTES_FAIXA_ETARIA_25_34_VALUE: '[data-testid="twentyFiveToThirtyFourYearsTested"]',
+    TESTES_FAIXA_ETARIA_35_45_VALUE: '[data-testid="thirtyFiveToFourtyFiveYearsTested"]',
+    TESTES_FAIXA_ETARIA_46_64_VALUE: '[data-testid="FourtySixToSixtyFourYearsTested"]',
+    TESTES_FAIXA_ETARIA_OUTRANGE_VALUE: '[data-testid="outRangeTested"]',
+    NEGATIVE_REASON_NAO_REALIZOU_VALUE: '[data-testid="labelAndValueNaoRealizou"]',
+    NEGATIVE_REASON_NAO_REALIZOU_PERCENT: '[data-testid="percentNaoRealizou"]',
+    NEGATIVE_REASON_MENSTRUADA_VALUE: '[data-testid="labelAndValueMenstruada"]',
+    NEGATIVE_REASON_MENSTRUADA_PERCENT: '[data-testid="percentMenstruada"]',
+    NEGATIVE_REASON_FORA_FAIXA_ETARIA_VALUE: '[data-testid="labelAndValueNotAge"]',
+    NEGATIVE_REASON_FORA_FAIXA_ETARIA_PERCENT: '[data-testid="percentNotAge"]',
+    NEGATIVE_REASON_HISTERECTOMIZADA_VALUE: '[data-testid="labelAndValueHisterectomizada"]',
+    NEGATIVE_REASON_HISTERECTOMIZADA_PERCENT: '[data-testid="percentHisterectomizada"]',
+    NEGATIVE_REASON_OUTROS_VALUE: '[data-testid="labelAndValueOthers"]',
+    NEGATIVE_REASON_OUTROS_PERCENT: '[data-testid="percentOthers"]'
+
+
+
+
+    
+    
 }
